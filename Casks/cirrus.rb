59b653a075c42097ee0672bd80f20c99ca2e4e61
Matthew Stickler <sticklerm3@me.com>
2019-02-08 16:13:00 -0600

    style-fix- corrected all formula's & fixed URL comments

--- Casks/cirrus.rb
+++ Casks/cirrus.rb
@@ -1,12 +1,13 @@
-cask "cirrus" do
-  version "1.3"
-  sha256 "5496c3fee46e8ba18e2d0943e44fcf24c6e95e1d27421696cc29345e903986cb"
+cask 'cirrus' do
+  version '1.3'
+  sha256 '5496c3fee46e8ba18e2d0943e44fcf24c6e95e1d27421696cc29345e903986cb'
 
-  url "https://eclecticlightdotcom.files.wordpress.com/2018/11/cirrus13.zip"
-  name "Cirrus"
-  homepage "https://eclecticlight.co/"
+  # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
+  url 'https://eclecticlightdotcom.files.wordpress.com/2018/11/cirrus13.zip'
+  name 'Cirrus'
+  homepage 'https://eclecticlight.co/'
 
-  depends_on :macos => ">= 10.11"
+  depends_on macos: '>= 10.11'
 
-  app "cirrus13/Cirrus.app"
+  app 'cirrus13/Cirrus.app'
 end
