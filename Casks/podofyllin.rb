cd541175eb433d4f82b01a8d1227a59dcebe2967
Matthew Stickler <sticklerm3@me.com>
2019-03-08 13:12:20 -0600

    Updated podofyllin to version 1.0b6

--- Casks/podofyllin.rb
+++ Casks/podofyllin.rb
@@ -1,13 +1,13 @@
 cask 'podofyllin' do
-  version '1.0b3'
-  sha256 'a269618c15b2942d5b7e3ee5489e8e2fdefd8c4cfd74bb55be53ac66a85ae0fe'
+  version '1.0b6'
+  sha256 '1f0b4f32318f7dd7eb129be7f27555533eac0d129e1f90e0e8aa1a872ce3f46c'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2019/02/podofyllin10b3.zip'
+  url 'https://eclecticlightdotcom.files.wordpress.com/2019/03/podofyllin10b6.zip'
   name 'Podofyllin'
   homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :sierra'
 
-  app 'podofyllin10b3/Podofyllin.app'
+  app 'podofyllin10b6/Podofyllin.app'
 end
