f6059175f18b42905f49568d750849dd5c27098a
Matthew Stickler <sticklerm3@me.com>
2019-04-08 11:49:39 -0500

    Bump lockrattler to v. 4.19

--- Casks/lockrattler.rb
+++ Casks/lockrattler.rb
@@ -1,13 +1,13 @@
 cask 'lockrattler' do
-  version '4.18'
+  version '4.19,2019.04'
   sha256 '185f01ddc8f054b85006867a2513528196c0b4190413cf2b2c88f4f2c1031db2'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2018/12/lockrattler418.zip'
-  name 'LockRattler'
-  homepage 'https://eclecticlight.co/2018/12/11/lockrattler-4-18-should-now-highlight-new-updates/'
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.dots_to_slashes}/lockrattler#{version.before_comma.no_dots}.zip"
+  name 'Lock Rattler'
+  homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :el_capitan'
 
-  app 'lockrattler418/LockRattler.app'
+  app "lockrattler#{version.before_comma.no_dots}/LockRattler.app"
 end
