3f1df646ae77e577d334d20788b3e5938ff52050
Matt <sticklerm3@me.com>
2019-04-12 12:26:02 -0500

    Upgrade archicecht to v1.0 (#41)
    also updated `app`, `url` & `version` stanzas for easier maintenanece in 
    future!

--- Casks/archichect.rb
+++ Casks/archichect.rb
@@ -1,15 +1,15 @@
 cask 'archichect' do
-  version '1.0b1'
-  sha256 '7b43947fdd314027d3a8dfbac4fe9b78ebd11bf9644e44b25f26f102bc7644d5'
+  version '1.0,2019.04'
+  sha256 '6dd3eaba09b198164153b14bbc43f43624e39cd769b4f16c512e197749604a20'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2019/04/archichect10b1.zip'
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.dots_to_slashes}/archichect#{version.before_comma.major}#{version.before_comma.minor}.zip"
   name 'ArchiChect'
   homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :sierra'
 
-  app 'archichect10b1/ArchiChect.app'
+  app "archichect#{version.before_comma.major}#{version.before_comma.minor}/ArchiChect.app"
 
-  caveats "Additional information about how to use #{token} can be found at #{staged_path}"
+  caveats "Additional documentation about #{token} and its usage can be found at #{staged_path}"
 end
