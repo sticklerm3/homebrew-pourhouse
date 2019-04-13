79239e923a079aa7e8c064ad6605e380384fc732
Matt <sticklerm3@me.com>
2019-04-13 07:12:54 -0500

    Update taccy.rb online

--- Casks/taccy.rb
+++ Casks/taccy.rb
@@ -1,7 +1,7 @@
 cask 'taccy' do
   version '1.0b7,2019.01'
   sha256 '3c6beef36c7230f29d8852e63bc0a7999be4829f2bcb8790dc29df961296e54c'
-
+  
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.dots_to_slashes}/taccy#{version.before_comma.no_dots}.zip"
   name 'Taccy'
