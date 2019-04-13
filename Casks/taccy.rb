a81f9966664a21224fb05673510b49477e96dd87
Matthew Stickler <sticklerm3@me.com>
2019-04-12 20:00:26 -0500

    fixes to taccy.rb
    - fixed typo in `version` stanza
    - updated formula to semantic versioning for easier maintenance
    - added `caveats` stanza pointing to app documentation

--- Casks/taccy.rb
+++ Casks/taccy.rb
@@ -1,13 +1,15 @@
 cask 'taccy' do
-  version '1.20b7'
+  version "1.0b7,2019.01"
   sha256 '3c6beef36c7230f29d8852e63bc0a7999be4829f2bcb8790dc29df961296e54c'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2019/01/taccy10b7.zip'
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.dots_to_slashes}/#{version.before_comma.major}#{version.before_comma.minor}.zip"
   name 'Taccy'
   homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :sierra'
 
-  app 'taccy10b7/Taccy.app'
+  app "taccy#{version.before_comma.major}#{version.before_comma.minor}/Taccy.app"
+  
+  caveats "Additional documentation about #{token} and its usage can be found at #{staged_path}"
 end
