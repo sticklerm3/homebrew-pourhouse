26da5e6ce89ffb65834cdac4087be6cb64d0aa66
Matthew Stickler <sticklerm3@me.com>
2020-01-18 17:22:55 -0600

    Upgrade searchkeylite to v1.3

--- Casks/searchkeylite.rb
+++ Casks/searchkeylite.rb
@@ -1,9 +1,9 @@
 cask 'searchkeylite' do
   version '1.3'
-  sha256 '9e8b05b23a3e8ff3f5b240a7250b63ea73cbd94c4eed4ed5033055c12d2397b4'
+  sha256 '7fc8bf4de5bc26ae4340355662e217680e5f660eaa79dee1ebebab8d20b8ccda'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2019/08/searchkey13.zip'
+  url 'https://eclecticlightdotcom.files.wordpress.com/2019/08/searchkeylite13.zip'
   name 'SearchKeyLite'
   homepage 'https://eclecticlight.co/downloads/'
 
