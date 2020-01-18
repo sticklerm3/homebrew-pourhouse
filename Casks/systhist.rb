d72903e123b760fc5054ad7a2fa1d880ea53e3ee
Matt <sticklerm3@me.com>
2020-01-18 16:28:51 -0600

    Updated  systhist to v1.12 (#55)

--- Casks/systhist.rb
+++ Casks/systhist.rb
@@ -1,15 +1,15 @@
 cask 'systhist' do
-  version '1.8,2019.04'
-  sha256 '22c9fdc401ab521fc7e2d388174ec8a474cbd5dccfbf18f4c375b5e8dadb81a5'
+  version '1.12'
+  sha256 '1b459b246051aa2f422557f014a2732b2783b82f3b8dc458bda0000f478c9e9e'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/systhist#{version.before_comma.no_dots}.zip"
+  url 'https://eclecticlightdotcom.files.wordpress.com/2019/06/systhist112.zip'
   name 'Syst Hist'
   homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :el_capitan'
 
-  app "systhist#{version.before_comma.major}#{version.before_comma.minor}/SystHist.app"
+  app 'systhist112/SystHist.app'
 
   caveats "Additional documentation about #{token} and its usage can be found at #{staged_path}"
 end
