321d197c9cb6940278236eae5394e50f17b4b2ad
Matt <sticklerm3@me.com>
2020-01-18 15:30:45 -0600

    Upgrade nalaprop to v1.0b10 (#51)

--- Casks/nalaprop.rb
+++ Casks/nalaprop.rb
@@ -1,13 +1,13 @@
 cask 'nalaprop' do
-  version '1.0b9'
-  sha256 '86534b14f3eb6b0aeab4ed0a783cb5e9291fa558546c8bffd4ccba6b7f274bae'
+  version '1.0b10'
+  sha256 'ff12cf7f859ca20b3e919c604c5dfd55bbe6ce2b55fa0af41744c609f65de200'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2018/11/nalaprop10b9.zip'
+  url 'https://eclecticlightdotcom.files.wordpress.com/2019/06/nalaprop10b10.zip'
   name 'Nalaprop'
   homepage 'https://eclecticlight.co/downloads/'
 
-  depends_on macos: :mojave
+  depends_on macos: '>= :mojave'
 
-  app 'nalaprop10b9/Nalaprop.app'
+  app 'nalaprop10b10/Nalaprop.app'
 end
