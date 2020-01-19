a6e74bdca1a2fcb79b7829d2d865a782fff33455
Matthew Stickler <sticklerm3@me.com>
2020-01-18 23:24:11 -0600

    Update delighted to v2.0b2

--- Casks/delighted.rb
+++ Casks/delighted.rb
@@ -1,15 +1,15 @@
 cask 'delighted' do
-  version '1.2'
-  sha256 'c04bd111dc261c1602f02101610eae25fb5672a45aaec53125970ef36718c072'
+  version '2.0b2'
+  sha256 '14d941a249eacee14a5ac00a49214da70cd6a8b0db75fba95b59f8aec9652690'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2019/04/delighted12.zip'
+  url 'https://eclecticlightdotcom.files.wordpress.com/2019/06/delighted20b2.zip'
   name 'DelightEd'
   homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :sierra'
 
-  app 'DelightEd12/DelightEd.app'
+  app 'DelightEd20b2/DelightEd.app'
 
   caveats "Additional documentation about #{token} and its usage can be found at #{staged_path}"
 end
