81debcbdc793700a9cb782613fef512f4b1caf45
Matthew Stickler <sticklerm3@me.com>
2020-01-19 20:03:59 -0600

    Update keychaincheck2 to v2.0b1*
    Also put name stanza in double quotes to test hound ci linting
    *

--- Casks/keychaincheck2.rb
+++ Casks/keychaincheck2.rb
@@ -1,13 +1,13 @@
 cask 'keychaincheck2' do
-  version '2.0a4'
-  sha256 '2c2140b7045ea3f8141d05b27a8390c99ed01ae37cca972b7c724871e91e615b'
+  version '2.0b1'
+  sha256 '787be5ccb676efe6ce0274b99d4d410fb87ad4c4e1f742224761d9d0c425925d'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2018/06/keychaincheck204.zip'
-  name 'KeychainCheck2'
+  url 'https://eclecticlightdotcom.files.wordpress.com/2019/08/keychaincheck21.zip'
+  name "KeychainCheck2"
   homepage 'https://eclecticlight.co/'
 
   depends_on macos: '>= :sierra'
 
-  app 'keychaincheck204/KeychainCheck2.app'
+  app 'keychaincheck21/KeychainCheck2.app'
 end
