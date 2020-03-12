7583ee28ea6754f5fb3430297647a31f912538bb
Matthew Stickler <sticklerm3@me.com>
2020-03-12 10:38:20 -0500

    update 32-bitcheck for interpolation

--- Casks/32-bitcheck.rb
+++ Casks/32-bitcheck.rb
@@ -1,13 +1,13 @@
 cask '32-bitcheck' do
-  version '1.8'
+  version '1.8,2019.06'
   sha256 'ce1bb088715c4071262e3970521fd4714fa77d44352c7bcf116e1069d37eb3eb'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2019/06/32bitcheck18.zip'
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/32bitcheck#{version.before_comma.major}#{version.before_comma.minor}.zip"
   name '32-bitCheck'
-  homepage 'https://eclecticlight.co/'
+  homepage 'https://eclecticlight.co/32-bitcheck-archichect/'
 
   depends_on macos: '>= :el_capitan'
 
-  app '32bitCheck18/32-bitCheck.app'
+  app "32bitCheck#{version.before_comma.no_dots}/32-bitCheck.app"
 end
