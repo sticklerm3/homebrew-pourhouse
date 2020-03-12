8aed8356b20fc8d00d3569b488afd9ceab2ee320
Matthew Stickler <sticklerm3@me.com>
2020-03-12 10:51:45 -0500

    upgrade cask stanzas

--- Casks/aquiline-check.rb
+++ Casks/aquiline-check.rb
@@ -1,13 +1,13 @@
 cask 'aquiline-check' do
-  version '1.0b2a'
+  version '1.0b2a,2018.09'
   sha256 'd00984c9391880e65119717edddaf17d957d45c97ecc2b47b67927bf1022e47f'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2018/09/aquiline10b2a.zip'
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/aquiline10b2a.zip"
   name 'Aquiline Check'
   homepage 'https://eclecticlight.co/downloads/'
 
-  depends_on macos: '>= :el_capitan'
+  depends_on macos: [:el_capitan, :sierra, :high_sierra]
 
-  app 'aquiline10b2a/Aquiline Check.app'
+  app "aquiline#{version.before_comma.no_dots}/Aquiline Check.app"
 end
