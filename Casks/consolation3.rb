2ea3eab9089ac2a3f162674ada053bff3be773c9
Matthew Stickler <sticklerm3@me.com>
2020-03-26 00:50:09 -0500

    bump consolation3 to v3.9 (#108)

--- Casks/consolation3.rb
+++ Casks/consolation3.rb
@@ -1,13 +1,13 @@
 cask 'consolation3' do
-  version '3.7'
-  sha256 '6a40db817a4a756c38cdfc8d9f8ec40b3ad75a58f05fc785b4752d83e773c5e6'
+  version '3.9,2020.03'
+  sha256 '6843e26aa6fa572b610951e596edc97d08ed1bf42674f13c581d853997a9aafb'
 
   # eclecticlightdotcom.files.wordpress.com was verified as official when first introduced to the cask
-  url 'https://eclecticlightdotcom.files.wordpress.com/2020/01/consolation37.zip'
+  url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/consolation#{version.before_comma.no_dots}.zip"
   name 'Consolation3'
-  homepage 'https://eclecticlight.co/'
+  homepage 'https://eclecticlight.co/consolation-t2m2-and-log-utilities/'
 
   depends_on macos: '>= :sierra'
 
-  app 'consolation37/Consolation3.app'
+  app "consolation#{version.before_comma.no_dots}/Consolation3.app"
 end
