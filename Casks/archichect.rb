de47010571d9a262f285791e4b9af85768ae9bb5
Dario Vladovic <d.vladimyr@gmail.com>
2020-08-29 03:12:58 +0200

    Update archichect from 2.1 to 2.3
    Add `zap` stanza.

--- Casks/archichect.rb
+++ Casks/archichect.rb
@@ -1,15 +1,21 @@
-cask 'archichect' do
-  version '2.1,2019.12'
-  sha256 '9e3bc457d73612fb2719f246dfe584583e3bc147ebbf05e8ad24fbd0acfe7b28'
+cask "archichect" do
+  version "2.3,2020.06"
+  sha256 "1562a04549db10bc57f5c736a79c4ffcb361728dc98d97e31220d9c78909f8b8"
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/archichect#{version.before_comma.no_dots}.zip"
-  name 'ArchiChect'
-  homepage 'https://eclecticlight.co/32-bitcheck-archichect/'
+  name "ArchiChect"
+  homepage "https://eclecticlight.co/32-bitcheck-archichect/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   app "archichect#{version.before_comma.no_dots}/ArchiChect.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/co.eclecticlight.archichect.sfl2",
+    "~/Library/Preferences/co.eclecticlight.ArchiChect.plist",
+    "~/Library/Saved Application State/co.eclecticlight.ArchiChect.savedState",
+  ]
 
   caveats "Additional documentation about #{token} and its usage can be found at #{staged_path}"
 end
