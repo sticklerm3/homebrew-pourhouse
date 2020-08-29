c76f0e62bf3052612796156c4410ea9ee5f1c022
Dario Vladovic <d.vladimyr@gmail.com>
2020-08-29 02:41:00 +0200

    Update alifix
    Add `zap` stanza.

--- Casks/alifix.rb
+++ Casks/alifix.rb
@@ -1,15 +1,20 @@
-cask 'alifix' do
-  version '1.1,2019.08'
-  sha256 '0a31ebab4ae988aee8fe1fd34f0bd2d894217e668d16fcfb4e086d12d410f1c4'
+cask "alifix" do
+  version "1.1,2019.08"
+  sha256 "0a31ebab4ae988aee8fe1fd34f0bd2d894217e668d16fcfb4e086d12d410f1c4"
 
   # eclecticlightdotcom.files.wordpress.com/ was verified as official when first introduced to the cask
   url "https://eclecticlightdotcom.files.wordpress.com/#{version.after_comma.major}/#{version.after_comma.minor}/alifix#{version.before_comma.no_dots}.zip"
-  name 'alifix'
-  homepage 'https://eclecticlight.co/taccy-signet-precize-alifix-utiutility-alisma/'
+  name "alifix"
+  homepage "https://eclecticlight.co/taccy-signet-precize-alifix-utiutility-alisma/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
   app "alifix#{version.before_comma.no_dots}/Alifix.app"
+
+  zap trash: [
+    "~/Library/Preferences/co.eclecticlight.Alifix.plist",
+    "~/Library/Saved Application State/co.eclecticlight.Alifix.savedState",
+  ]
 
   caveats do
     "Additional documentation about how to use #{token} can be found at #{staged_path}"
